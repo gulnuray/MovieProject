c9883578f1c9eec0f7a29827180d7a27eaff15b2
Humeyra Gul Nuray <nurayhumeyragul@gmail.com>
2026-02-25 13:44:13 +0300

    Cast mediator controller eklendi

--- Core/MovieApi.Application/Features/MediatorDesignPattern/Handlers/CastHandlers/GetCastByIdQueryHandler.cs
+++ Core/MovieApi.Application/Features/MediatorDesignPattern/Handlers/CastHandlers/GetCastByIdQueryHandler.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using MediatR;
+using MovieApi.Application.Features.MediatorDesignPattern.Queries.CastQueries;
+using MovieApi.Application.Features.MediatorDesignPattern.Results.CastResults;
+using MovieApiPersistence.Context;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +10,28 @@ using System.Threading.Tasks;
 
 namespace MovieApi.Application.Features.MediatorDesignPattern.Handlers.CastHandlers
 {
-	public class GetCastByIdQueryHandler
+	public class GetCastByIdQueryHandler : IRequestHandler<GetCastByIdQuery, GetCastByIdQueryResult>
 	{
+		private readonly MovieContext _context;
+
+		public GetCastByIdQueryHandler(MovieContext context)
+		{
+			_context = context;
+		}
+
+		public async Task<GetCastByIdQueryResult> Handle(GetCastByIdQuery request, CancellationToken cancellationToken)
+		{
+			var values = await _context.Casts.FindAsync(request.CastId);
+			return new GetCastByIdQueryResult
+			{
+				Biography = values.Biography,
+				CastId = values.CastId,
+				ImageUrl = values.ImageUrl,
+				Name = values.Name,
+				Overwiew = values.Overwiew,
+				Surname = values.Surname,
+				Title = values.Title
+			};
+		}
 	}
 }
---
+++ Core/MovieApi.Application/Features/MediatorDesignPattern/Results/TagResults/GetTagByIdQuery.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieApi.Application.Features.MediatorDesignPattern.Results.TagResults
+{
+	public class GetTagByIdQuery
+	{
+		public int TagId { get; set; }
+
+		public string Title { get; set; }
+	}
+}
