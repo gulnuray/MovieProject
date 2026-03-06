65db4ce1c0702a0cb4fbe5b24369d694053226f0
Humeyra Gul Nuray <nurayhumeyragul@gmail.com>
2026-03-06 16:04:39 +0300

    Layout Partial Viewlara Ayrıldı (bitmedi)

---
+++ Frontends/MovieApi.WebUI/ViewComponents/UserLayoutWebUIViewComponents/_UserLayoutWebUILoginModelComponentPartial.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MovieApi.WebUI.ViewComponents.UserLayoutWebUIViewComponents
+{
+	public class _UserLayoutWebUILoginModelComponentPartial:ViewComponent
+	{
+		public IViewComponentResult Invoke()
+		{
+			return View();
+		}
+
+	}
+}
