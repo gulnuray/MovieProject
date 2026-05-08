8b5926360f5ce52b115b97eb67a5111914db0deb
Humeyra Gul Nuray <nurayhumeyragul@gmail.com>
2026-05-08 13:40:44 +0300

    MovieList sayfası yüklendi

---
+++ Frontends/MovieApi.WebUI/ViewComponents/UserLayoutWebUIViewComponents/_UserLayoutWebUIHeroComponentPartial.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MovieApi.WebUI.ViewComponents.UserLayoutWebUIViewComponents
+{
+	public class _UserLayoutWebUIHeroComponentPartial:ViewComponent
+	{
+		public IViewComponentResult Invoke()
+		{
+			return View();
+		}
+
+	}
+}
